758b61dbade860a96b8bf0b894536f68eb2858c7
Pranjali <pranjalis241@gmail.com>
2025-03-02 21:57:18 +0530

    initial commit

---
+++ demo/src/components/Registration.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Registration = () => {
+    return (
+        <div className="container mt-5">
+            <h2>Registration</h2>
+            <p>Register here.</p>
+        </div>
+    );
+};
+
+export default Registration;
